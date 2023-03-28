abefa3573097dbb02cae42e2cd083ce87d67da0c
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-03-28 20:20:52 +0200

    add switch case

--- src/Main.java
+++ src/Main.java
@@ -14,16 +14,33 @@ public class Main {
     // меню
     // список функций
     // создать класы для каждой функцыии
-    //сделать клас задача добавить кампаратор
+    // сделать клас задача добавить кампаратор
     // добавить енам для команд меню
     //
 
     public static void main(String[] args) {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         ArrayList<String> todoList = new ArrayList<String>();
-        System.out.println(" would you like to do");
+        System.out.println(" Добро пожаловать в список задач ");
+        int option = Integer.parseInt(br.readLine());
+                switch () {
+                    case 1:
+                        System.out.println("Просмотреть список задач : ");
+
+                        break;
+                    case 2:
+                        System.out.println("Добавить новою задачу : ");
+                        break;
+                    case 3:
+
+                        System.out.println("Удалить задачу ");
+                        break;
+                    case 4:
+                        System.out.println("Выйти из програми ");
+                        break;
 
 
+                }
 
 
     }
