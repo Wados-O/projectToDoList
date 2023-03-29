85f3454e9cf18a796d5231e8e9d89417bfeace04
Roman Varanita <romanvaranita@gmail.com>
2023-03-29 18:59:26 +0200

    Add method File Writer

--- src/ToDoList.java
+++ src/ToDoList.java
@@ -1,5 +1,8 @@
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+
+import static jdk.internal.org.jline.utils.ShutdownHooks.tasks;
 
 public class ToDoList {
   private List<String> task;
@@ -8,6 +11,28 @@ public class ToDoList {
   public void TodoList(String fileName) {
     this.fileName = fileName;
     this.task = new ArrayList<>();
+    readTasksFromFile();
+  }
+
+  private void readTaskFromFile() {
+    try {
+      BufferedReader br = new BufferedReader(new FileReader(fileName));
+      String task;
+      while ((task = br.readLine()) !=null) {
+        tasks.add(task);
+      }
+      br.close();
+    } catch (IOException e) {
+      System.out.println(" Failed to read task from file: " + e.getMessage());
+    }
+  }
+  private void writeTaskToFile() {
+    try {
+      BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+      for (String task : tasks) {
+        writer.write(task + "\n");
+      }
+    }
   }
 
 
