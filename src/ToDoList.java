140acce00ec673cc8a5eaf5a11eaddf8746c7d52
Roman Varanita <romanvaranita@gmail.com>
2023-03-29 18:35:59 +0200

    Add method TodoList

--- src/ToDoList.java
+++ src/ToDoList.java
@@ -1,4 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ToDoList {
+  private List<String> task;
+  private String fileName;
+
+  public void TodoList(String fileName) {
+    this.fileName = fileName;
+    this.task = new ArrayList<>();
+  }
 
 
 }
