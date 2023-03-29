3f6e4469b1e740c3334ec6c94badcb4e407352d8
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-03-29 18:25:24 +0200

    add todo test

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
@@ -16,27 +17,42 @@ public class Main {
     // создать класы для каждой функцыии
     // сделать клас задача добавить кампаратор
     // добавить енам для команд меню
+
+
     //
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         ArrayList<String> todoList = new ArrayList<String>();
         System.out.println(" Добро пожаловать в список задач ");
+        System.out.println(" 1.Просмотреть список задач : ");
+        System.out.println(" 2.Добавить новою задачу :");
+        System.out.println(" 3.Удалить задачу ");
+        System.out.println(" 4.Выйти из програми ");
         int option = Integer.parseInt(br.readLine());
-                switch () {
+
+                String itemToAdd = null;
+
+
+
+                switch (option) {
                     case 1:
                         System.out.println("Просмотреть список задач : ");
-
+                        itemToAdd = br.readLine();
                         break;
                     case 2:
                         System.out.println("Добавить новою задачу : ");
+                        itemToAdd = br.readLine();
+
                         break;
                     case 3:
 
                         System.out.println("Удалить задачу ");
+                        itemToAdd = br.readLine();
                         break;
                     case 4:
                         System.out.println("Выйти из програми ");
+                        itemToAdd = br.readLine();
                         break;
 
 
